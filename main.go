1b948ea078eb598336f0fb00953b9238fc5a5a87
Shakil Abdin <shakil.abdin@gmail.com>
2020-10-01 19:55:16 -0400

    more

--- main.go
+++ main.go
@@ -2,18 +2,26 @@ package main
 
 import "fmt"
 
-func main()  {
+var y = 20 + 16
+
+// declare the value of variable to be interger/0
+var z int
+
+func main() {
+	// := short decleration operator
+	// declare a variable and assign a value
 	x := 8
 	fmt.Println(x)
-	
+
 	x = 24
 	fmt.Println(x)
-	
-	y := 20 + 16
+
+	// var can be used to declare a variable outside the func ln 5
+	// var y = 20 + 16
 	fmt.Println(y)
-	
-	z := 199 * 45
+
+	// declared on line 8
+	z = 199 * 45
 	fmt.Println(z)
 
-	
 }
